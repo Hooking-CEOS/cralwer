d245e50660d9ec7852e9a871e44c64496d6e2c88
moong <moonki0623@naver.com>
2023-05-26 22:40:21 +0900

    [fix]: Login Logic Fix

--- hk_crawler.py
+++ hk_crawler.py
@@ -1,10 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
 import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
 
 # 환경변수 불러오기
 load_dotenv()
@@ -12,6 +16,9 @@ load_dotenv()
 # 브라우저 꺼짐 방지 옵션
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
+
+# 개발 끝나고 실제 크롤링할 때는 해당 내용 주석 제거 필요
+# chrome_options.headless = True
 
 # 불필요한 에러 메시지 삭제
 chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
@@ -21,7 +28,11 @@ service = Service(executable_path=ChromeDriverManager().install())
 
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-driver.get('https://www.instagram.com/')
+# brandID = 인스타 브랜드 아이디
+brandID = 'freshian.official'
+
+driver.get(f'https://www.instagram.com/accounts/login?next=%2F{brandID}%2F&source=desktop_nav')
+
 # driver.get('https://www.instagram.com/freshian.official/')
 
 driver.implicitly_wait(20)
@@ -43,3 +54,11 @@ userpw_input.send_keys(os.environ.get("Instagram_PW"))
 submit_btn = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
 submit_btn.click()
 
+driver.implicitly_wait(10)
+
+authInfo_btn = driver.find_element(By.CSS_SELECTOR, "button[type='button']");
+authInfo_btn.click()
+
+
+driver.close()
+driver.quit()
